f1e3f3f4ed17849a1cf66e27cf51c9630a3a8cfb
samoffem <samoffem@gmail.com>
2025-01-24 14:36:58 +0100

    added header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '@/images/logo.png'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Searchbar from './Searchbar'
 
 const Header = () => {
   return (
     <div className='border-b'>
-      <div>
-        <div>
-          <Link href="/">
+      <div className='flex flex-col lg:flex-row items-center gap-4 p-4'>
+        <div className='flex items-center justify-between w-full lg:w-auto'>
+          <Link href="/" className='font-bold shrink-0'>
             <Image 
               src={logo}
               alt="logo"
@@ -16,6 +18,82 @@ const Header = () => {
               className='w-24 lg:w-28'
             />
           </Link>
+
+          <div className='lg:hidden'>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            <SignedOut>
+              <SignInButton mode="modal">
+
+                <button className="bg-gray-100 text-gray-800 px-3 py-1.5
+                  text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300
+                ">
+                    Sign In
+                </button>
+
+              </SignInButton>
+            </SignedOut>
+
+          </div>
+        </div>
+        <div className='w-full lg:max-w-2xl'>
+          <Searchbar />
+        </div>
+        <div className='hidden lg:block ml-auto'>
+          <SignedIn>
+            <div className="flex items-center gap-3">
+              <Link href="/seller">
+                <button className='bg-blue-600 text-white px-3 py-1.5
+                  text-sm rounded-lg hover:bg-blue-700 transition
+                '>
+                  Sell Tickets
+                </button>
+              </Link>
+              <Link href="/tickets">
+                <button className='bg-gray-100 text-gray-800 px-3 py-1.5
+                  text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300
+                '>
+                  My Tickets
+                </button>
+              </Link>
+              <UserButton />
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+
+              <button className="bg-gray-100 text-gray-800 px-3 py-1.5
+                text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300
+              ">
+                  Sign In
+              </button>
+
+            </SignInButton>
+          </SignedOut>
+
+        </div>
+
+        <div className='lg:hidden w-full flex justify-center gap-3'>
+          <SignedIn>
+            <div className="flex items-center gap-3">
+              <Link href="/seller">
+                <button className='bg-blue-600 text-white px-3 py-1.5
+                  text-sm rounded-lg hover:bg-blue-700 transition
+                '>
+                  Sell Tickets
+                </button>
+              </Link>
+              <Link href="/tickets">
+                <button className='bg-gray-100 text-gray-800 px-3 py-1.5
+                  text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300
+                '>
+                  My Tickets
+                </button>
+              </Link>
+            </div>
+          </SignedIn>
         </div>
       </div>
 

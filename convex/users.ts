d01bde6148adb813a52ab2b356e1428fad19d6c7
Samuel Offem <samoffem@gmail.com>
2025-06-22 14:58:42 +0100

    created the user model

---
+++ convex/users.ts
@@ -0,0 +1,32 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const updateUser = mutation({
+    args: {
+        userId: v.string(),
+        email: v.string(),
+        name: v.string()
+    },
+    handler: async (ctx, {userId, email, name})=>{
+        //check for existing user
+        const existingUser = await ctx.db
+            .query("users")
+            .withIndex("by_user_id", (q)=> q.eq("userId", userId))
+            .first()
+        if(existingUser){
+            await ctx.db.patch(existingUser._id, {
+                name, email
+            })
+            return existingUser._id
+        }
+
+        const newUser = await ctx.db.insert("users", {
+            userId,
+            name,
+            email,
+            stripeConnectId: undefined
+        }) 
+        return newUser
+        
+    }
+})

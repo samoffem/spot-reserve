c242f9a0d6276e0c2de3e611443445f8213d4a46
Samuel Offem <samoffem@gmail.com>
2025-07-07 15:22:34 +0100

    changes

--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -6,9 +6,23 @@ import { useQuery } from "convex/react"
 const EventsList = () => {
     const events = useQuery(api.events.get)
     console.log(events)
+
+    if(!events){
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    const upcomingEvents = events.filter(event=> event.eventDate > Date.now())
+        .sort((a,b)=> a.eventDate - b.eventDate)
+
+    const pastEvent = events.filter(event=> event.eventDate <= Date.now())
+        .sort((a, b)=> b.eventDate - a.eventDate)
   return (
     <div>EventsList</div>
   )
 }
+
+
 
 export default EventsList
